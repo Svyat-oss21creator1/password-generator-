eda98b4684d7a0566e50fb1436bd36c95b28f2bc
Svyat Cherepinsky <svyatoslavcherepinsky@gmail.com>
2022-08-05 14:03:23 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,144 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Checkbox from './components/Checkbox';
 
 function App() {
+  const [password,setPassword] = useState({
+    length:5,
+    uppercase:false,
+    lowercase:false,
+    numbers:false,
+    symbols:false,
+  });
+  const [handleText,sethandleText] = useState('');
+  const[copied,setCopied] =useState(false);
+
+  const handleChangeUppercase =()=>{
+    setPassword({
+      ...password,
+      uppercase:!password.uppercase,
+    });
+  }
+  const handleChangeLowercase =()=>{
+    setPassword({
+      ...password,
+      lowercase:!password.lowercase,
+    });
+  }
+  const handleChangeNumbers = () => {
+    setPassword({
+      ...password,
+      numbers: !password.numbers,
+    });
+  };
+  const handleChangeSymbols = () => {
+    setPassword({
+      ...password,
+      symbols: !password.symbols,
+    });
+  };
+  const setPasswordLength = (val) => {
+    setPassword({
+      ...password,
+      length: val,
+    });
+  };
+  function generatePassword() {
+    const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    const symbolsArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
+
+    const characterCodes = Array.from(Array(26)).map((_e, i) => i + 97);
+    const lowerCaseLetters = characterCodes.map((code) =>
+      String.fromCharCode(code)
+    );
+    const upperCaseLetters = lowerCaseLetters.map((letter) =>
+      letter.toUpperCase()
+    );
+    const { length, uppercase, lowercase, numbers, symbols } = password;
+    const generateTheWord = (
+      length,
+      uppercase,
+      lowercase,
+      numbers,
+      symbols,
+    ) => {
+      const availableCharacters = [
+        ...(lowercase ? lowerCaseLetters : []),
+        ...(uppercase ? upperCaseLetters : []),
+        ...(numbers ? numbersArray : []),
+        ...(symbols ? symbolsArray : []),
+      ];
+      const shuffleArray = (array) => array.sort(() => Math.random() - 0.5);
+      const characters = shuffleArray(availableCharacters).slice(0, length);
+      sethandleText(characters.join(''));
+      return characters;
+    };
+    generateTheWord(length, uppercase, lowercase, numbers, symbols);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="container wrapper-box">
+        <h2>Password Generator</h2>
+        <div className="password-box">
+          <input type='text' value={handleText} onChange={(e)=>sethandleText(e.target.value)}/>           
+          <button className='copy-button' onClick={()=>{
+            if(handleText.length >0){
+              navigator.clipboard.writeText(handleText);
+              setCopied(true);
+              setInterval(()=>{
+                setCopied(false);
+              },2000)
+            }
+          }}>{copied ? 'Copied!':"Copy text"}</button>         
+        </div>
+        <br />
+        <div className="word-crieteria__box">
+          <div>
+            <label>Password length</label>
+          </div>
+          <div>
+            <input type='number' value={password.length} onChange={(e)=>setPasswordLength(e.target.value)}/>
+          </div>
+        </div>
+        <div className="word-crieteria__box">
+          <div>
+            <label>Include uppercase letters</label>
+          </div>
+          <div>
+            <Checkbox value={password.uppercase} onChange ={handleChangeUppercase}/>           
+          </div>
+        </div>
+        <div className="word-crieteria__box">
+          <div>
+            <label>Include lowercase letters</label>
+          </div>
+          <div>
+            <Checkbox value={password.lowercase} onChange ={handleChangeLowercase}/>            
+          </div>
+        </div>
+        <div className="word-crieteria__box">
+          <div>
+            <label>Include numbers</label>
+          </div>
+          <div>
+            <Checkbox value={password.numbers} onChange ={handleChangeNumbers}/>            
+          </div>
+        </div>
+        <div className="word-crieteria__box">
+          <div>
+            <label>Include symbols</label>
+          </div>
+          <div>
+            <Checkbox value={password.symbols} onChange ={handleChangeSymbols}/>            
+          </div>
+        </div>
+        <div>
+          <button className="generate-button" onClick={generatePassword} >
+            Generate password
+          </button>
+        </div>
+      </div>      
     </div>
   );
 }
-
 export default App;
